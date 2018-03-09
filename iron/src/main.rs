0be24f682b75b918e986ea18c0b6563a9f8c3b94
vagrant <vagrant@ubuntu-14.04-amd64-vbox>
2018-03-09 15:01:59 +0000

    remove flont

--- iron/src/main.rs
+++ iron/src/main.rs
@@ -1,24 +1,20 @@
-#[macro_use]
-extern crate mysql;
-extern crate serde;
+
 extern crate serde_json;
-extern crate clam;
 extern crate params;
-#[macro_use]
-extern crate serde_derive;
 extern crate iron;
-use clam::template::TemplateBuilder;
-use std::collections::HashMap;
+extern crate router;
+use params::FromValue;
+use iron::status;
+use iron::headers::ContentType;
+use router::{Router};
 use iron::prelude::*;
+fn json(_: &mut Request) -> IronResult<Response> {
+    //let json: String = serde_json::to_string(data).unwrap();
+    Ok(Response::with((ContentType::html().0, status::Ok, "hi")))
+}
 fn main() {
-    fn top_handler(_: &mut Request) -> IronResult<Response> {
-        let mut data = HashMap::new();
-        data.insert("item", "hoge");
-        let html = TemplateBuilder::new("../public/html/iron.html")
-            .data(data)
-            .build()
-            .html();
-        html
-    }
-    let _server = Iron::new(top_handler).http("localhost:3000").unwrap();
+    let mut router = Router::new();
+    router.get("/", json, "json");
+    let _server = Iron::new(router).http("0.0.0.0:80").unwrap();
+    println!("lisning on localhost:8080");
 }
